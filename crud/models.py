759742146cc053e906f574bbbd86b8355fe24c0b
unknown <sogelloezreal49@gmail.com>
2026-05-04 20:00:48 +0800

    continuation in pt.3 28:48

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Genders(models.Model):
+    class Meta:
+        db_table = 'genders_tbl'
+    gender_id = models.BigAutoField(primary_key=True, blank=False)
+    gender = models.CharField(max_length=55, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Users(models.Model):
+    class Meta:
+        db_table = 'users_tbl'
+    user_id = models.BigAutoField(primary_key=True, blank=False)
+    full_name = models.CharField(max_length=64, blank=False)
+    gender = models.ForeignKey(Genders, on_delete=models.CASCADE)
+    birthdate = models.DateField(blank=False)
+    address = models.CharField(max_length=254, blank=False)
+    contact_number = models.CharField(max_length=16, blank=True)
+    email = models.CharField(max_length=128, blank=True)
+    username = models.CharField(max_length=64, blank=False, unique=True)
+    password = models.CharField(max_length=255, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
